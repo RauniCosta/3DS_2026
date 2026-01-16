ba14864624d42ea60cf60ed289081e495de39829
RauniCosta <rauni.costa@outlook.com.br>
2026-01-16 10:59:41 -0300

    Atualizado para Icones e representações de botões.

--- RauniCosta/exemploflutter/lib/pages/login_page.dart
+++ RauniCosta/exemploflutter/lib/pages/login_page.dart
@@ -14,7 +14,7 @@ class _LoginPageState extends State<LoginPage> {
       //SafeArea evita que elementos do sistema operacional interfiram na interface
       child: Scaffold(
         //Scaffold é a estrutura base de uma tela
-        backgroundColor: const Color(0xFFF0C8C8),
+        backgroundColor: const Color.fromARGB(255, 211, 211, 223),
         body: Container(
           width: double.infinity,
           //Container é um widget de layout que pode conter outros widgets
@@ -24,22 +24,66 @@ class _LoginPageState extends State<LoginPage> {
             children: [
               Container(
                 margin: EdgeInsets.only(top: 100),
-                width: 150, 
-                height: 150, 
-                color: Colors.amber
+                child: Icon(Icons.person, size: 150, color: Colors.blue),
               ),
+              SizedBox(height: 50),
               Container(
-                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                margin: EdgeInsets.symmetric(horizontal: 20),
                 color: Colors.blue,
-                height: 50,
-                width: double.infinity,
+                height: 30,
                 alignment: Alignment.center,
-                child: 
-                Text(
-                  'Login',
+                child: Text(
+                  'Email',
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                color: Colors.blue,
+                height: 30,
+                alignment: Alignment.center,
+                child: Text(
+                  'Senha',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(height: 350),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 30),
+                color: Color.fromARGB(255, 20, 124, 16),
+                height: 30,
+                width: 120,
+                alignment: Alignment.center,
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 30),
+                color: const Color.fromARGB(255, 211, 211, 223),
+                height: 30,
+                width: 120,
+                alignment: Alignment.center,
+                child: Text(
+                  'Cadastre-se',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
